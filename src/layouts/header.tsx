d26721e409a14c897a5d67a10ffdef782bb17996
odinaka-dev <uchechukwupeter22@gmail.com>
2025-12-15 19:44:30 +0100

    Updated route links

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,15 +1,22 @@
-import Logo from "@/components/common/logo"
-import { Button } from "@/components/ui/button"
+import Logo from "@/components/common/logo";
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 
 const Header = () => {
   return (
     <div>
-        <div className="flex  justify-between items-start sm:items-baseline gap-4">
-            <Logo />
-            <Button className="lg:text-sm text-[10px] lg:py-6 lg:px-8 rounded-full cursor-pointer hover:bg-[#07C75F] bg-[#07C75F] text-white">Contact Us</Button>
-        </div>
+      <div className="flex  justify-between items-start sm:items-baseline gap-4">
+        <Link to={"https://myrtleng.com/"}>
+          <Logo />
+        </Link>
+        <Link to={"https://myrtleng.com/contact-us"}>
+          <Button className="lg:text-sm text-[10px] lg:py-6 lg:px-8 rounded-full cursor-pointer hover:bg-[#07C75F] bg-[#07C75F] text-white">
+            Contact Us
+          </Button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
